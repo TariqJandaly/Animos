9af357834107adba6d56fbeaec031c530c5a2bb1
TariqJandaly <tariq.jandaly@gmail.com>
2023-08-10 13:47:24 +0300

    Added better nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Search from "./Search"
 
 const Navbar = () => {
   return (
-    <div className="md:flex block justify-between px-10">
+    <div className="md:flex md:pb-0 pb-5 bg-gradient-to-b from-slate-800 via-slate-800 via-80% to-transparent sticky top-0 block justify-between px-10 pt-5 z-50">
         <Link href={ '/' } className="text-white text-2xl md:mb-0 mb-3">Anima</Link>
         <Search />
         <div></div>
